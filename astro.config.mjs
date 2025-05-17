9f9e48c73765a318fbd71a88d9f105a53caa7b99
Parth Sinha <sinhaparth555@gmail.com>
2025-05-17 13:07:48 +0100

    added project parth

---
+++ astro.config.mjs
@@ -0,0 +1,29 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import cloudflare from '@astrojs/cloudflare';
+
+import mdx from '@astrojs/mdx';
+
+import sitemap from '@astrojs/sitemap';
+import tailwindcss from '@tailwindcss/vite';
+import siteConfig from './src/data/site-config';
+
+// https://astro.build/config
+export default defineConfig({
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true
+    }
+  }),
+  site: siteConfig.website,
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  integrations: [mdx(), sitemap({
+    customPages: ["https://projects.parthsinha.com/blogs", "https://projects.parthsinha.com/projects"],
+    changefreq: 'weekly',
+    priority: 0.7,
+    lastmod: new Date(),
+  })]
+});
